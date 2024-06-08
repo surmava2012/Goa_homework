499d2e30c487f30bff658378c21351295ed7fa3e
SubjectGX <nanainatan@gmail.com>
2024-06-08 04:18:07 -0200

    first commit

--- Level 01/homework/Level 01.py
+++ Level 01/homework/Level 01.py
@@ -1,4 +1,5 @@
 name = "Aleksandre"
 surname = "Surmava"
 age = 11
-print(" my name is " + name +" im " + str(age) " yars old " )
+height = 72.8
+print(" my name is " + name +" im " + str(age) + " my height is " + str(height) )
