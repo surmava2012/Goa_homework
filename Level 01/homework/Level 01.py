d6d3501edb5640c59586417b0e6ddfdcbc9fa6f2
SubjectGX <qweqwe@gmail.com>
2024-06-09 06:34:17 -0200

    first commit

--- Level 01/homework/Level 01.py
+++ Level 01/homework/Level 01.py
@@ -2,4 +2,8 @@ name = "Aleksandre"
 surname = "Surmava"
 age = 11
 height = 72.8
-print(" my name is " + name +" im " + str(age) + " my height is " + str(height) )
+hobby = "socer"
+country = "Georgia"
+city = "Kutaisi"
+mood = "funny"
+print(" my name is " + name +" im " + str(age) +" yars old " +  " my height is " + str(height) + " my hobby is " + hobby + " my country " + country + " my city " + city)
