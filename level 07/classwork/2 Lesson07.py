3e85cb7225a6e3cfea5fb793b47216380596aa90
SubjectGX <qweqwe@gmail.com>
2024-06-27 12:57:39 -0200

    commit

---
+++ level 07/classwork/2 Lesson07.py
@@ -0,0 +1,59 @@
+# coding consist 3 steps
+
+# 1) writing
+# 2) execute
+# 3) debugging
+
+
+
+# კოდირება მოიცავს 3 ეტაპს:
+
+# 1) კოდის წერა
+# 2) კოდის გაშვება
+# 3) გაშვებული კოდის შემოწმობა(თუ ქარმოიქმნა შეცდომა)
+
+# input
+
+# name = input("Enter Your Name: ")
+# last_name = input("Enter Your Last-Name:")
+# country = input("Which country do u live:")
+# print(name,last_name,country)
+
+# print(" my name is " + name + " my last name is " + last_name + " I live a country  " + country)
+
+
+# age = input("Enter your age: fd2")
+# print(age)
+
+# type()
+# str()
+# int()
+
+
+# name = "Aleksandre"
+# age = 11
+
+# print(type(name))
+# print(type(age))
+
+
+
+# age = input("Enter your age:")
+
+# print(type(age))
+
+
+
+
+
+x = int(input("enter x: "))
+y = int(input("enter y: "))
+
+print(x + y)
+
+
+int()
+str()
+type()
+print()
+input()
