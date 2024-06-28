938ed51b821d3a5a894165cc8d651088b2431665
SubjectGX <qweqwe@gmail.com>
2024-06-28 10:57:51 -0200

    commit

---
+++ level 07/homework/03 Lesson 07.py
@@ -0,0 +1,11 @@
+name = "Aleksandre"
+surname = "Surmava"
+age = 11
+height = 72.8
+hobby = "socer"
+country = "Georgia"
+city = "Kutaisi"
+favorite_color = "green"
+favorite_car_model = "TOYOTA"
+favorite_food = "Khachapuri"
+print(" my name is " + name + " my lastname is " + surname + " im " + str(age) + " yars old " +  " my height is " + str(height) + " my hobby is " + hobby + " my country " + country + " my city " + city + " my favorite color is " + favorite_color + " my favorite car model is " + favorite_car_model + " my favorite food is " + favorite_food)
