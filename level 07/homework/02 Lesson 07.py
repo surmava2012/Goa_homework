ca6601a3fe3bb730ababf02e34c6650a6cde3657
SubjectGX <qweqwe@gmail.com>
2024-07-02 15:46:48 -0200

    commit

--- level 07/homework/02 Lesson 07.py
+++ level 07/homework/02 Lesson 07.py
@@ -1,1 +1,5 @@
-print(int(input("რამდენი წლის არის შენი დედა: " )) + int(input("ჩაწერე 20: ")))
+dad_age = int(input("Enter Dad age: "))
+
+after_20_yars = dad_age + 20
+
+print("after 20 years your father age will be ", after_20_yars, "years old")
---
+++ level 07/homework/homework.py
@@ -0,0 +1,61 @@
+# 1) sololearn module 2 quiz ჩათვლით
+
+# ===========================================
+
+# 2) მომხმარებელს შემოატანინეთ 4 სხავდასხვა რიცხვი და ამ ოთხივე რიცხვზე მოახდინეთ არითმეტიკური ოპერაციები. +,-,*,//,**
+
+# num1 = int(input("Enter num1: "))
+# num2 = int(input("Enter num2: "))
+# num3 = int(input("Enter num3: "))
+# num4 = int(input("Enter num4: "))
+
+# print(num1 + num2 + num3 + num4)
+# print(num1 - num2 - num3 - num4)
+# print(num1 * num2 * num3 * num4)
+# print(num1 // num2 // num3 // num4)
+
+
+# ============================================================
+
+
+# 3) დაწერეთ პროგრამა სადაც შემოიტანთ თქვენი ოჯახის წევრების ასაკს და გამოიტანეთ ის თუ რამდენი წლის იქნებიან 20 წლის შემდეგ.
+
+# პირველი ეტაპი: მომხმარებელს შემოვატანიოთ ოჯახის წევრის ასაკი
+# გავიგოთ თუ რამდენი წლის იქნება 20 წლის შემდეგ
+
+# dad_age = int(input("Enter Dad age: "))
+
+# after_20_yars = dad_age + 20
+
+# print("after 20 years your father age will be ", after_20_yars, "years old")
+
+# ================================================================================
+
+# 4) შექმენით ცვლადები სადაც შეინახავთ თქვენს თავზე ინფორმაციას (სახელი, გვარი, ასაკი, ქვეყანა, ქალაქი, საყვარელი ფერი, საყვარელი მანქანა, საყვარელი საჭმელი, საყვარელი სპორტი და ა.შ) შემდეგ შემოტანილი მნიშვნელობებით ააწყვეტ ერთი დიდი წინადადება.
+
+# name = "Aleksandre"
+# surname = "Surmava"
+# age = 11
+# height = 72.8
+# hobby = "socer"
+# country = "Georgia"
+# city = "Kutaisi"
+# favorite_color = "green"
+# favorite_car_model = "TOYOTA"
+# favorite_food = "Khachapuri"
+# print(" Hello! my name is " + name + " my lastname is " + surname + " im " + str(age) + " yars old " +  " my height is " + str(height) + " my hobby is " + hobby + " my country " + country + " my city " + city + " my favorite color is " + favorite_color + " my favorite car model is " + favorite_car_model + " my favorite food is " + favorite_food)
+
+# ================================================================================
+
+# 5) დაწერეთ მარტივი პროგრამა, რომელიც სთხოვს მომხმარებელს შეიყვანოს თავისი სახელი და შემდეგ დაბეჭდოს მისალოცი შეტყობინება მათი სახელის გამოყენებით.
+
+# name = input("enter your name: ")
+# print("gilocav!", name)
+
+# ================================================================================
+
+# 6) შექმენით ცვლადი რომელშიც შეინახავთ ნივთის ფასს. მეორე ცვლადში შეინახეთ ნივთების რაოდენობა. თქვენი დავალებაა, რომ დაბეჭდოთ, თუ რამდენი უნდა გადაიხადოთ ნივთების ყიდვისას (ფასი * რაოდენობა)
+
+# item_price = int(input("enter your price"))
+# items_sum = int(input("enter your sum"))
+# print("ჩვენ უნდა სულ გადავიხადოთ: ", item_price * items_sum )
