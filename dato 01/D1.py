2ccc2498d68fb22fef52f0443f185dc73e311732
SubjectGX <qweqwe@gmail.com>
2024-07-05 14:01:38 -0200

    commit

---
+++ dato 01/D1.py
@@ -0,0 +1,19 @@
+name = "Dato"
+surname = "Surmava"
+country = "Georgia"
+age = 59
+
+
+print(" Hello my name is " + name + " my surname is " + surname + " my country is " + country + " my age is " + str(age))
+# =======================
+
+name2 = "Soso"
+surname2 = "Surmava"
+country2 = "Georgia"
+age2 = 58
+print(" Hello my name is " + name2 + " my surname is " + surname2 + " my country is " + country2 + " my age is " + str(age2))
+
+# ====================
+# age3 = age + age2
+
+print(" ორივე ასაკი ერთად არის  " + str(age + age2))
