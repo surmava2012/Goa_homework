800a580c4241bc9109e688fb9f978030a08403dd
SubjectGX <qweqwe@gmail.com>
2024-07-07 00:09:04 -0200

    commit

---
+++ Level 09/classwork/2 Lesson 09.py
@@ -0,0 +1,10 @@
+# Convert the values into numbers
+wins = input()
+ties = input()
+
+# 1 win = 3 poimts
+# 1 tie = 1 point
+# Calculate the score
+score = 7
+
+# Concatenate the score
---
+++ Level 09/classwork/Lesson 09.py
@@ -0,0 +1,70 @@
+# asks the user to enter the savings
+savings = input()
+
+# Convert the user input into a float value and update the vatiable
+savings = 150
+
+# Savings grow after 1 year at a 5% annual interset rate
+balance = savings * 1.05
+
+# Convert the balance unto a string and uodate the variable
+balance = 143.8
+
+# Concatenate the 2 strings to produce a message
+message = "Amout in 1 year: " + balance
+
+# Display the message
+
+print(message)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Convert the values into numbers
+wins = input()
+ties = input()
+
+# 1 win = 3 poimts
+# 1 tie = 1 point
+# Calculate the score
+score = 7
+
+# Concatenate the score
