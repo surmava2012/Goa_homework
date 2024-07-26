72643e8b7d82374bf92174f52eb9617439bc17ec
SubjectGX <qweqwe@gmail.com>
2024-07-26 11:02:52 -0200

    commit

---
+++ Level 11/classwork/Lesson.py
@@ -0,0 +1,30 @@
+# Logical operators
+# 1) 'and' operator
+
+#ლოგიჯური ოპერაცია დეტალებში
+# 1) მნიშვნელობა
+# 2) ოპერატორი
+# 3) მნიშვნეობა
+
+
+# and operator
+
+# print(True and True) # True
+# print(True and False) # False
+# print(False and True) # False
+# print(False and False) # False
+
+
+
+
+
+
+# or operator
+
+#  (1) (2) (3)
+# print(True or True) # True
+# print(True or False) # False
+# print(False or True) # False
+# print(False or False) # False
+
+
---
+++ Level 11/homework/HomeWork2.py
@@ -0,0 +1,8 @@
+age = int(input("შეიყვანეთ თქვენი ასაკი: "))
+
+# ასაკის შედარება 18-ზე
+if age >= 18:
+    print(True)
+else:
+    print(False)
+
