6418ed283bbd2183aac4b114cda82d48996bf47b
SubjectGX <qweqwe@gmail.com>
2024-08-07 06:45:56 -1200

    commit

---
+++ Level 14/classwork/Vereficaition.py
@@ -0,0 +1,9 @@
+user_age = 1
+
+enter = int(input("If you are 18 years old, write one, if not, then any number: "))
+
+print(user_age == enter)
+ask2 = 1
+
+ask = int(input('If you are a student, write one, if not, then any number: '))
+print(ask2 == ask)
