461a8ce3d6a9fbe3139d9f0fd7c95d28601dafdf
SubjectGX <qweqwe@gmail.com>
2024-08-11 10:15:10 -1200

    commit

---
+++ Level 04/homework/Task0.py
@@ -0,0 +1,9 @@
+name = "Aleksandre"
+surname = "Surmava"
+age = 11
+height = 72.8
+hobby = "socer"
+country = "Georgia"
+city = "Kutaisi"
+mood = "funny"
+print(" my name is " + name + " im " + str(age) + " yars old " +  " my height is " + str(height) + " my hobby is " + hobby + " my country " + country + " my city " + city)
